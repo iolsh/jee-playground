368c0c5a07e09f690224ac8b571a6fe9eab52424
Jacek Olszewski <jacek.olszewski@gmail.com>
2019-11-17 16:23:54 +0100

    add exception handling

--- src/main/java/me/iolsh/application/security/SecurityFilter.java
+++ src/main/java/me/iolsh/application/security/SecurityFilter.java
@@ -1,6 +1,7 @@
 package me.iolsh.application.security;
 
 import io.jsonwebtoken.Jwts;
+import me.iolsh.exceptions.RestExceptionTemplate;
 
 import javax.annotation.Priority;
 import javax.inject.Inject;
@@ -18,7 +19,10 @@ import java.security.Key;
 @Secure
 @Priority(Priorities.AUTHENTICATION)
 public class SecurityFilter implements ContainerRequestFilter {
+
     private final String BEARER = "Bearer";
+    private final String ERR_MSG = "Token invalid.";
+    private final String SOLUTION = "Provide valid token.";
 
     @Inject
     Security security;
@@ -34,7 +38,8 @@ public class SecurityFilter implements ContainerRequestFilter {
         try {
             Jwts.parser().setSigningKey(key).parseClaimsJws(token);
         } catch (Exception e) {
-            requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
+            requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).
+                entity(new RestExceptionTemplate(ERR_MSG, SOLUTION)).build());
         }
     }
 }
