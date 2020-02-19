277c99bfe123ba917b21f8da61bf8422bca0b1b8
Jacek Olszewski <jacek.olszewski@gmail.com>
2020-02-19 19:40:13 +0100

    add sample interceptor

--- src/main/java/me/iolsh/repository/BookRepository.java
+++ src/main/java/me/iolsh/repository/BookRepository.java
@@ -1,12 +1,15 @@
 package me.iolsh.repository;
 
 import me.iolsh.entity.Book;
+import me.iolsh.inerceptors.PerformanceMonitor;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 
 import static javax.transaction.Transactional.TxType.REQUIRED;
 
+
+@PerformanceMonitor
 @ApplicationScoped
 @Transactional(REQUIRED)
 public class BookRepository extends AbstractHibernateRepository<Book, String> {
