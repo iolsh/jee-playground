349d611609c945e5c833244483e2515003e0ba18
Jacek Olszewski <jacek.olszewski@gmail.com>
2020-03-01 11:32:12 +0100

    restructure packaging - part 3

--- src/main/java/me/iolsh/users/boundry/InvalidCredentialsException.java
+++ src/main/java/me/iolsh/users/boundry/InvalidCredentialsException.java
@@ -1,4 +1,19 @@
 package me.iolsh.users.boundry;
 
-public class InvalidCredentialsException extends RuntimeException {
+import me.iolsh.infrastructure.exceptions.RestExceptionTemplate;
+
+import javax.ejb.ApplicationException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+@ApplicationException
+public class InvalidCredentialsException extends WebApplicationException {
+
+    private static final String MSG = "Invalid credentials.";
+    private static final String SOLUTION = "Either register or provide valid user/password.";
+
+    public InvalidCredentialsException() {
+        super(Response.status(Response.Status.UNAUTHORIZED).entity(new RestExceptionTemplate(MSG, SOLUTION)).build());
+    }
+
 }
