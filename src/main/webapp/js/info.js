f85309a4f62dc0524b6fa86bba79dd461bb2e828
Jacek Olszewski <jacek.olszewski@gmail.com>
2020-03-02 15:11:55 +0100

    add bootstrap

--- src/main/webapp/js/info.js
+++ src/main/webapp/js/info.js
@@ -7,10 +7,15 @@ class Info extends HTMLElement {
 
     connectedCallback() {
         this.root.innerHTML = `
-            <h1>Hello from JEE Playground!</h1>
-            See <a href="http://localhost:8080/openapi/">Api docs</a> or 
-            <a href="http://localhost:8080/micro-playground/api/openapi-ui/">Swagger UI</a> 
-            for things to play with.</p>
+            <link rel="stylesheet" href="css/bootstrap.min.css"/>
+            <div class="container">
+            <h3>Hello from JEE Playground!</h3>
+            <div class="alert alert-primary" role="alert">
+            See <a class="btn btn-dark btn-sm" href="http://localhost:8080/openapi/" role="button">Api docs</a> or 
+            <a class="btn btn-dark btn-sm" href="http://localhost:8080/micro-playground/api/openapi-ui/" role="button">Swagger UI</a> 
+            for things to play with.
+            </div>
+            </div>
         `
     }
 }
