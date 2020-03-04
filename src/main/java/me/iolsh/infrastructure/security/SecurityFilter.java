949de25619cccd559756648169d4181ae02539ef
Jacek Olszewski <jacek.olszewski@gmail.com>
2020-03-04 20:33:56 +0100

    corrections

--- src/main/java/me/iolsh/infrastructure/security/SecurityFilter.java
+++ src/main/java/me/iolsh/infrastructure/security/SecurityFilter.java
@@ -37,7 +37,6 @@ public class SecurityFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-        System.out.println("Auth HEADER: " + authHeader);
         if (authHeader == null || !authHeader.startsWith(BEARER)) {
             throw new NotAuthorizedException("No valid token found");
         }
